58b5dbe0ace0e863be63061346406eaab3c7fb9d
subrata153 <subrata.goswami153@gmail.com>
2020-09-13 16:36:09 +0530

    added create ingredient feature

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -18,4 +18,4 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ('id', 'name')
-        read_only_fields = ('id')
+        read_only_fields = ('id', )
--- recipe/views.py
+++ recipe/views.py
@@ -27,7 +27,7 @@ class TagViewSet(viewsets.GenericViewSet,
         
 
 
-class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
     """Manage ingredients in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated, )
@@ -37,3 +37,8 @@ class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """Return objects for the current authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+    def perform_create(self, serializer):
+        """Create a new igredient"""
+        serializer.save(user=self.request.user)
