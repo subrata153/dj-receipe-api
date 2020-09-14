7d981fe6ee594517225329bf9a32a25a52a5d1ee
subrata153 <subrata.goswami153@gmail.com>
2020-09-14 11:40:33 +0530

    added list recipe feafure, added custom recipe detail view

--- recipe/views.py
+++ recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 from recipe import serializers
 
@@ -76,3 +76,29 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
 #     def perform_create(self, serializer):
 #         """Create a new igredient"""
 #         serializer.save(user=self.request.user)
+
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """manage recipes in the database"""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+
+    def get_queryset(self):
+        """Retrive the recipes for the authenticated user"""
+        return self.queryset.filter(user=self.request.user)
+
+    #default class form django rest
+    def get_serializer_class(self):  
+        """Return appropriate serializer class"""
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+
+        return self.serializer_class    
+
+
+    def perform_create(self, serializer):
+        """Create a new reciepe"""
+        serializer.save(user=self.request.user)        
